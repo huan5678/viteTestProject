81abdaaed29ec7618e156db3a8c9e8e38f0af1ab
huan5678 <huan5678@gmail.com>
2021-09-16 17:29:39 +0800

    tailwind rwd setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: { min: '576px' },
+      md: { min: '768px' },
+      lg: { min: '992px' },
+      xl: { min: '1200px' },
+      '2xl': { min: '1400px' },
+    },
     extend: {},
   },
   variants: {
